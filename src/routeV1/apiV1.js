e6f6583e87cdb7135717aa995b7a691883f6d286
doanviethung1009 <doanviethung1009@gmail.com>
2025-06-05 09:52:34 +0700

    #2 update api

--- src/routeV1/apiV1.js
+++ src/routeV1/apiV1.js
@@ -7,12 +7,12 @@ const routerAPI = express.Router();
 
 const apiV1 = (app) => {
     routerAPI.get("/", (req, res) => {
-        // res.json({
-        //     name: "hung",
-        //     function: "test"
-        // })
-        res.send('Hello World!')
-        console.log(">>> check action")
+        res.json({
+            name: "hung",
+            function: "test"
+        })
+        // res.send('Hello World!')
+        // console.log(">>> check action")
     })
 
     return app.use("/v1/api", routerAPI)
